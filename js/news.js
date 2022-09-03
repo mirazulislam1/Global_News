b4cf86a52df0cf4c3f3b18c0decf1cc0546c7048
unknown <you@example.com>
2022-09-03 21:59:28 +0600

    full modal added

--- js/news.js
+++ js/news.js
@@ -39,11 +39,14 @@ const displayCategorys = categorys =>{
           <div class="col-md-8">
             <div class="card-body">
               <h5 class="card-title fs-2">${title}</h5>
-              <p class="card-text fs-5">${details.length > 400?details.slice(0, 400) +' ...' :details}</p>
+              <p class="card-text fs-5">${details.length >400?details.slice(0, 400) +' ...' :details}</p>
               <div class="d-flex justify-content-between">
              <div> <img src="${author.img}" class="rounded-circle author" alt="...">
-             <a>${author.name ?author.name :'No name'}</a></div>
-              <p>${total_view ?total_view: 'No view'}</p>
+             <a>${author.name ?author.name :'No data found'}</a></div>
+             <p><span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+             <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+             <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+           </svg></span> ${total_view ? total_view: 'No data found'}</p>
               <button href="#" class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="loadModalNews('${value._id}')" >show Details</button 
               </div>
             </div>
@@ -67,6 +70,14 @@ const displayCategorys = categorys =>{
   const modalBody = document.getElementById('modal-body')
   modalBody.innerHTML = `
   <img src="${value.image_url}" class="img-fluid rounded-start " alt="...">
+  <p class="card-text">${value.details}</p>
+  <div class="d-flex justify-content-between">
+  <div> <img src="${value.author.img}" class="rounded-circle author" alt="...">
+  <a>${value.author.name ? value.author.name :'No data found'}</a></div>
+  <p> <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+  <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+  <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+</svg></span> ${value.total_view ? value.total_view: 'No data found'}</p></div>
   `
  
   
