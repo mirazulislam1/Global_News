bd44129ddfb574d53cbe6a530e484a6696272f70
unknown <you@example.com>
2022-09-03 13:25:37 +0600

    image and title added

--- js/news.js
+++ js/news.js
@@ -30,15 +30,15 @@ const displayCategorys = categorys =>{
         console.log(value);
         const div = document.createElement('div')
         div.innerHTML= `
-        <div class="card mb-3 p-5 m-4" >
+        <div class="card mb-3 p-4 m-4" >
         <div class="row g-0">
           <div class="col-md-4">
-            <img src="..." class="img-fluid rounded-start" alt="...">
+            <img src="${value.thumbnail_url}" class="img-fluid rounded-start" alt="...">
           </div>
           <div class="col-md-8">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <h5 class="card-title">${value.title}</h5>
+              <p class="card-text">${value.details.slice(0, 400) +' ...'}</p>
               <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
             </div>
           </div>
