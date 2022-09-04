6796457a65f96485453f525f57f86c14aea14a4e
unknown <you@example.com>
2022-09-04 06:58:40 +0600

    error added

--- js/news.js
+++ js/news.js
@@ -1,8 +1,13 @@
 const loadCategorys = async() =>{
     const url = `https://openapi.programming-hero.com/api/news/categories`
-    const res = await fetch(url);
-    const data = await res.json();
-    displayCategorys(data.data.news_category)
+    try{
+        const res = await fetch(url);
+        const data = await res.json();
+        displayCategorys(data.data.news_category)
+    }
+    catch (error){
+        console.log(error);
+    }
 
 }
 const displayCategorys = categorys =>{
@@ -23,29 +28,35 @@ const displayCategorys = categorys =>{
 }
  const loadCategorysDetails =async (category_id) =>{
     const url= `https://openapi.programming-hero.com/api/news/category/${category_id}`
+try{
     const res = await fetch(url);
     const data = await res.json()
     displayCategorysDetails(data.data)
+  }
+  catch (error){
+    console.log(error)
+  }
  }
+ 
  const displayCategorysDetails = category =>{
          // start spinner
          toggleSpinner(true);
-    const cardContainer = document.getElementById('card-container')
-      
+    const cardContainer = document.getElementById('card-container')  
+     
     cardContainer.textContent='';
     category.forEach(value =>{
-      const {image_url, title, details, author,total_view } = value
+      const {thumbnail_url, title, details, author,total_view } = value
         console.log(value);
         const div = document.createElement('div')
         div.innerHTML= `
-        <div class="card mb-3 border border-0 shadow-lg" >
+        <div class="card mb-3 p-2 border border-0 shadow-lg" >
         <div class="row g-0">
           <div class="col-md-4">
-            <img src="${image_url}" class="img-fluid rounded-start " alt="...">
+            <img src="${thumbnail_url}" class="img-fluid rounded-start " alt="...">
           </div>
           <div class="col-md-8">
             <div class="card-body">
-              <h5 class="card-title">${title}</h5>
+              <h5 class="card-title fs-3">${title}</h5>
               <p class="card-text">${details.length >400?details.slice(0, 400) +' ...' :details}</p>
               <div class="d-flex justify-content-between">
              <div> <img src="${author.img}" class="rounded-circle author" alt="...">
@@ -68,9 +79,14 @@ const displayCategorys = categorys =>{
  }
       const loadModalNews =async(news_id) =>{
       const url =` https://openapi.programming-hero.com/api/news/${news_id}`
+  try{
       const res = await fetch(url)
       const data = await res.json()
       displayNewsDetails(data.data[0])
+  }
+  catch (error){
+    console.log(error);
+  } 
  }
  const displayNewsDetails = value =>{
   console.log(value)
